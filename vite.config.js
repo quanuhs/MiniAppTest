36972cd9c0cb07566288eee8b6ee21953487b7b4
Gleb <37934662+quanuhs@users.noreply.github.com>
2024-07-19 13:42:32 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   base: "/testTG/",
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./testTG/src', import.meta.url))
     }
   }
 })
